1f837a53ad4198a8ca0e7bae4cf5234ea9cb79a6
Yi Wang <logins.yiwang@gmail.com>
2020-12-19 00:40:40 -0500

    Add genre picker

---
+++ FaveFlicks/Movie/GenrePicker.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct GenrePicker: View {
+  
+  @Binding var genre: String
+  
+  var body: some View {
+    Picker(selection: $genre, label: Spacer(), content: {
+      ForEach(Movie.possibleGenres, id: \.self) {
+        Text($0)
+      }
+    }).pickerStyle(WheelPickerStyle())
+  }
+}
+
+struct GenrePicker_Previews: PreviewProvider {
+  static var previews: some View {
+    GenrePicker(genre: .constant("Action"))
+  }
+}
--- FaveFlicks/Movie/MovieList.swift
+++ FaveFlicks/Movie/MovieList.swift
@@ -1,9 +1,15 @@
+/**
+ If you’re trying to decide whether to use @State, @StateObject or @ObservedObect, there are two questions you can ask about the property you’re declaring:
+
+ Is it a value type or a reference type? If you’re working with a value type, use @State.
+ Should SwiftUI manage the lifecycle of the property? If you’re only using the object in the view the property is declared in, @StateObject works fine. But, if the object is created or used outside the view, then @ObservedObject is a better match.
+ */
 import SwiftUI
 
 // swiftlint:disable multiple_closures_with_trailing_closure
 struct MovieList: View {
   
-  @ObservedObject var movieStore = MovieStore()
+  @StateObject var movieStore = MovieStore()
   @State private var isPresented = false
 
   var body: some View {
