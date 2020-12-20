031e1422b029346bc0e28b8a14516753541c565f
Yi Wang <logins.yiwang@gmail.com>
2020-12-20 01:12:20 -0500

    comments

--- FaveFlicks/User/UserStore.swift
+++ FaveFlicks/User/UserStore.swift
@@ -1,1 +1,14 @@
-import Foundation
+// https://www.raywenderlich.com/11781349-understanding-data-flow-in-swiftui#toc-anchor-014
+/**
+ To use an environment object, you need to do the following:
+ Create a class conforming to ObservableObject.
+ Have at least one variable in the class with the @Published property wrapper to trigger any observers to update, or manually provide an objectWillChange publisher as required by ObservableObject.
+ Pass an instance of the observable class to a view by using the environmentObject() view modifier when creating the view.
+ */
+import Combine
+
+class UserStore: ObservableObject {
+  // The @Published property wrapper is what triggers any updates in observers of an ObservableObject
+  @Published var currentUserInfo: UserInfo?
+  
+}
